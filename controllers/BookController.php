f17fee3cce46fc172215d420be2eb84ec399824e
voii83 <voiivoii83@gmail.com>
2019-07-19 12:41:39 +0300

    GET:Получение всех книг данного автора (в формате json)

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -2,7 +2,10 @@
 
 namespace app\controllers;
 
+use app\models\Book;
 use yii\rest\ActiveController;
+use yii\rest\IndexAction;
+use yii\web\NotFoundHttpException;
 
 class BookController extends ActiveController
 {
@@ -21,5 +24,26 @@ class BookController extends ActiveController
                 ],
             ],
         ]);
+    }
+
+    public function actions()
+    {
+        return [
+            'index' => [
+                'class' => IndexAction::className(),
+                'modelClass' => $this->modelClass,
+                'checkAccess' => [$this, 'checkAccess'],
+                'prepareDataProvider' => function ($action) {
+                    $model = new Book();
+                    $author = \Yii::$app->request->queryParams;
+                    $result = $model->search($author);
+
+                    if (!$result) {
+                        throw new NotFoundHttpException;
+                    }
+                    return $result;
+                }
+            ],
+        ];
     }
 }
--- models/Book.php
+++ models/Book.php
@@ -2,12 +2,55 @@
 
 namespace app\models;
 
+use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
 
 class Book extends ActiveRecord
 {
     public static function tableName()
     {
         return 'books';
+    }
+
+    public function rules()
+    {
+        return [
+            [['year_issue', 'id_author'], 'integer'],
+            [['title', 'edition'], 'string'],
+        ];
+    }
+
+    public function behaviors()
+    {
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+            ],
+        ];
+    }
+
+    public function getAuthor()
+    {
+        return $this->hasOne(Author::className(), ['id' => 'id_author']);
+    }
+
+    public function fields()
+    {
+        return ArrayHelper::merge(parent::fields(), [
+            'author',
+        ]);
+    }
+
+    public function search($author)
+    {
+        if ($author) {
+            return Book::find()->where(['id_author' => $author])->all();
+        }
+        return Book::find()->all();
     }
 }
